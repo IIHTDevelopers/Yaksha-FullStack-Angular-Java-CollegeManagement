a62489212ceab799a55ba8e9b26e4716fa71a113
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-09-01 17:11:05 +0530

    Update Student.java

--- Backend/src/main/java/com/collegemanagement/entity/Student.java
+++ Backend/src/main/java/com/collegemanagement/entity/Student.java
@@ -1,15 +1,5 @@
 package com.collegemanagement.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 public class Student {
 	
 	private Long id;
@@ -54,4 +44,5 @@ public class Student {
 	}
 
 }
+
 
